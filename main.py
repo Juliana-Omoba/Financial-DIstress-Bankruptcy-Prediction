ff7c05221cf3e15c98d14a05249ecc79a95da7bd
Juliana-Omoba <adeolaa33@gmail.com>
2023-04-14 19:18:57 -0600

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print ("Hello World!")
-print (" This is Python Programming Git and Git Hub repository")
+print ("Hello My World!")
+print (" This is Python Programming Git and Git Hub repository Assignment")
 print("We would use this repository for our assignment")
 
 print("I will know it")
